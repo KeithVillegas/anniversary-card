383893b28f6244e5883ef8c6bc2aefa6519546db
Keith Villegas <keithvillegas22@gmail.com>
2024-09-19 12:07:09 +0800

    Version-1 landing page

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import './components/quiz/quiz-component.js';
 import './components/landing-page/landing-page.js';
+import './components/messages/messages.js';
 
 const loadLandingPage = async () => {
     try {
@@ -14,7 +15,20 @@ const loadLandingPage = async () => {
     }
 };
 
-const loadQuizComponent = async () => {
+const loadMessages = async () => {
+    try {
+        const response = await fetch('./components/messages/messages.html');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const html = await response.text();
+        document.getElementById('messages').innerHTML = html;
+    } catch (err) {
+        console.error('Error loading messages page:', err);
+    }
+};
+
+const loadQuiz = async () => {
     try {
         const response = await fetch('./components/quiz/quiz-component.html');
         if (!response.ok) {
@@ -28,4 +42,5 @@ const loadQuizComponent = async () => {
 };
 
 loadLandingPage();
-loadQuizComponent();
+loadQuiz();
+loadMessages();
